7e246b42ac50c54a45150f6616dd99c5ce2ccaa7
Jen <jennifer.stemkowski@gmail.com>
2024-04-18 11:05:10 -0400

    Add initial ui for user info

---
+++ generator.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+
+root = Tk()
+root.title("CV Generator")
+
+
+# Name
+label_name = Label(root, text="Name")
+label_name.pack()
+entry_name = Entry(root)
+entry_name.pack()
+
+
+# Email
+label_email = Label(root, text="Name")
+label_email.pack()
+entry_email = Entry(root)
+entry_email.pack()
+
+
+# Phone Number
+label_phone = Label(root, text="Name")
+label_phone.pack()
+entry_phone = Entry(root)
+entry_phone.pack()
+
+
+# Address
+label_address = Label(root, text="Name")
+label_address.pack()
+entry_address = Entry(root)
+entry_address.pack()
+
+
+# Website
+label_website = Label(root, text="Name")
+label_website.pack()
+entry_website = Entry(root)
+entry_website.pack()
+
+root.mainloop()
