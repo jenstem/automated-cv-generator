f7f2f9a9500b110ef735a32f411ac83283dee80e
Jen <jennifer.stemkowski@gmail.com>
2024-04-18 11:13:47 -0400

    Adjust text titles for entry fields

--- generator.py
+++ generator.py
@@ -13,28 +13,28 @@ entry_name.pack()
 
 
 # Email
-label_email = Label(root, text="Name")
+label_email = Label(root, text="Email")
 label_email.pack()
 entry_email = Entry(root)
 entry_email.pack()
 
 
 # Phone Number
-label_phone = Label(root, text="Name")
+label_phone = Label(root, text="Phone Number")
 label_phone.pack()
 entry_phone = Entry(root)
 entry_phone.pack()
 
 
 # Address
-label_address = Label(root, text="Name")
+label_address = Label(root, text="Address")
 label_address.pack()
 entry_address = Entry(root)
 entry_address.pack()
 
 
 # Website
-label_website = Label(root, text="Name")
+label_website = Label(root, text="Website")
 label_website.pack()
 entry_website = Entry(root)
 entry_website.pack()
